d1932c32b1682c0046b8bab351b4ec8c29fccaf2
Oblutack <kamenjas.evvel@gmail.com>
2025-11-13 20:51:25 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- python-ai-service/main.py
+++ python-ai-service/main.py
@@ -18,7 +18,7 @@ class TextPayload(BaseModel):
     text: str
 
 llm = ChatOpenAI(
-    model="llama3-8b-8192", 
+    model="llama3-70b-8192", 
     api_key=os.getenv("GROQ_API_KEY"), 
     base_url="https://api.groq.com/openai/v1"
 )
